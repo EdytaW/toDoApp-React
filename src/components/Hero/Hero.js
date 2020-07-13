cf73cc394cb5881b2ca4500c2057f6916c24750a
Edyta Wojdala <edytawoj095@gmail.com>
2020-07-13 20:26:32 +0200

    Add Column.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,7 +6,6 @@ const Hero = props => (
   <header className={styles.component}>
     <h2 className={styles.title}>{props.titleText}</h2>
     <img className={styles.image} src={props.imageProp}/>	 
-    
   </header>
 );	
 
