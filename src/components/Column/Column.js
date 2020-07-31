68682de718c80677e5c7685150bf838b81786221
Edyta Wojdala <edytawoj095@gmail.com>
2020-07-31 21:14:27 +0200

    Add Icon.js

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import {settings} from '../../data/dataStore';
 import Card from '../Card/Card.js';
 import Creator from '../Creator/Creator.js';
+import Icon from '../Icon/Icon.js';
 
 class Column extends React.Component {
   //zapisywał zawartość właściwości cards w stanie komponentu
@@ -17,10 +18,15 @@ class Column extends React.Component {
     id: PropTypes.string,
   } 
     render() {
-      const { title, cards, addCard } = this.props;
+      const { title, cards, addCard, icon } = this.props;
       return (
         <section className={styles.component}>
-          <h3 className={styles.title}>{title} </h3>
+          <h3 className={styles.title}>
+            <span className={styles.icon}>
+              <Icon name={icon} />
+            </span>
+            {title}
+          </h3>
           {cards.map((cardData) => (
             <Card key={cardData.id} {...cardData} />
           ))} 
